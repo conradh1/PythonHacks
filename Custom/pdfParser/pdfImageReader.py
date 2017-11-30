facaf5352bdbd88df87ff788ebcbfc3d7470dcfc
conradh <conradh1@gmail.com>
2017-11-30 13:41:56 -1000

    Added pdf reader example.

---
+++ Custom/pdfParser/pdfImageReader.py
@@ -0,0 +1,32 @@
+from wand.image import Image
+from PIL import Image as PI
+import pyocr
+import pyocr.builders
+import io
+
+
+tool = pyocr.get_available_tools()[0]
+lang = tool.get_available_languages()[0]
+
+req_image = []
+final_text = []
+
+
+image_pdf = Image(filename="./Toptenofeverything.pdf", resolution=300)
+image_jpeg = image_pdf.convert('jpeg')
+
+for img in image_jpeg.sequence:
+    img_page = Image(image=img)
+    req_image.append(img_page.make_blob('jpeg'))
+    
+for img in req_image: 
+    txt = tool.image_to_string(
+        PI.open(io.BytesIO(img)),
+        lang=lang,
+        builder=pyocr.builders.TextBuilder()
+    )
+   #print "debug"+txt
+    final_text.append(txt)
+    
+    
+print final_text[1]
