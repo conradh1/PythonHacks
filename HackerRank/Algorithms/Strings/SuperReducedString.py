a1daf2b90d6e1cc7f9fd9f19421b561369b956ed
conradh <conradh1@gmail.com>
2018-01-03 17:29:17 -1000

    Added simple casearCiper solution.

--- HackerRank/Algorithms/Strings/SuperReducedString.py
+++ HackerRank/Algorithms/Strings/SuperReducedString.py
@@ -4,9 +4,16 @@ import sys
 import re
 
 def super_reduced_string(s):
-    r = re.sub(r'(\w)\1{2}', "", s)
-    print r
+    p= re.compile(r'(\w)\1{1}',re.I)
+    m = p.search(s)
+    while(m):
+        s = p.sub("", s,count=1)
+	m= p.search(s)
+	
+    if ( s == ''):
+        s = 'Empty String'
+    return s
 
-s = input().strip()
+s = raw_input();
 result = super_reduced_string(s)
 print(result)
