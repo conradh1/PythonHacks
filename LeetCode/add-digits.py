fbb6bf2e35feb42c06ddfb805fce5f1ea24816a0
conradh1 <conradh1@gmail.com>
2021-03-06 11:19:58 -0700

    Added simple solution for add-digits problem but Leedcode give different answer from test runs?

---
+++ LeetCode/add-digits.py
@@ -0,0 +1,36 @@
+#!/bin/python
+#https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def add_digits(num):
+
+    digits = num
+    sum = num
+
+    while digits > 9:
+        sum = 0
+        for x in str(digits):
+            sum += int(x)
+        digits = sum
+
+    return sum
+
+
+
+
+if __name__ == '__main__':
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = str(raw_input())
+
+    res = add_digits(n)
+    print (str(res))
+    #fptr.write(str(res) + '\n')
+
+    #fptr.close()
