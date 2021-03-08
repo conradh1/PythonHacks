cdc9a5b1b6c53f81e66064b41f9a7b1353e4705b
conradh1 <conradh1@gmail.com>
2021-03-07 17:27:40 -0700

    Added power of two with binary string for Leetcode problem.

---
+++ LeetCode/power_two.py
@@ -0,0 +1,26 @@
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the checkMagazine function below.
+def powerTwo(num):
+    isTwo = False
+
+    if num < 0: return False
+    # convert to 32 bit binary in string 0101...
+    bin= list(format(num, "b").zfill(32))
+
+
+    if (bin.count('1') == 1):
+        isTwo = True
+
+    return isTwo
+if __name__ == '__main__':
+    num = int(raw_input().rstrip())
+
+    print(powerTwo(num))
