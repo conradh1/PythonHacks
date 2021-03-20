1e1bd07329f46710a3ff249458a4f7aa4faca302
conradh <conradh1@gmail.com>
2021-03-20 12:26:42 -0600

    Added non sort solution to runnerup.

--- HackerRank/BasicDataTypes/runnerup.py
+++ HackerRank/BasicDataTypes/runnerup.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python
 # Find the runner up
 
+# runner up with sort
 def runnerup(arr):
     arr.sort()
 
     if (len(arr) == 1):
         return arr[0]
     else:
-        top = arr[len(arr)-1]
-        for i in range(len(arr)-1,-1, -1):            
-            if arr[i] < top:
-                return arr[i]
-        return top
+        return arr[len(arr)-2]
 
+# runnerup without sort
+def runnerup2(arr):
+    if (len(arr) == 2):
+        if (arr[0] < arr[1]):
+            return arr[0]
+        else:
+            return arr[1]
+    else:
+        mx = arr[0]
+        my = arr[0]        
+        for i in range(0, len(arr)):
+            if ( arr[i] > mx):
+                mx= arr[i]
+            if (arr[i] > my and arr[i] < mx):
+                my = arr[i]
+                        
+    return my
 
 if __name__ == '__main__':
     n = int(raw_input())
     arr = map(int, raw_input().split())
-print(runnerup(arr))
+print(runnerup2(arr))
