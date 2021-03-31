19161ad5ef8a39868926c1354ea572f1936f52c7
conradh <conradh1@gmail.com>
2021-03-30 18:21:35 -0600

    Added brute force solution for search2Dmatrix solution.

--- LeetCode/search2Dmatrix.py
+++ LeetCode/search2Dmatrix.py
@@ -8,15 +8,24 @@ from collections import Counter
 def searchMatrix(matrix, target):
 
     # start at the middle
-    i = len(an_array)/2
-    j = len(an_array[0]/2
+    x = target
 
-    if 
+    i = len(matrix)/2
+        
+    for i in range (0,len(matrix)):        
+            if (x >= matrix[i][0] and x <= matrix[i][len(matrix[i])-1]):            
+                for j in range(0,len(matrix[i])):                
+                    if ( matrix[i][j] == x):
+                        return True
+                return False        
+            
     return False
+
 if __name__ == '__main__':
     target= int(raw_input().rstrip())
 
-    matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
-
-    result = searchMatrix(matrix, target)
+    matrix2 = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+    matrix1 = [1,2,3,4,5,6,7,8,9]
+    matrix2 = [[1],[3]]
+    result = searchMatrix(matrix2, target)
     print(result)
